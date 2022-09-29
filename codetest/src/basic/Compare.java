82fa1df2b02919ccc0a5b9707b45194ed46e8cff
leeangz <114210903+leeangz@users.noreply.github.com>
2022-09-29 14:32:09 +0900

    2022-09-29

---
+++ codetest/src/basic/Compare.java
@@ -0,0 +1,19 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Compare {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if (A > B) System.out.println(">");
+		else if ( A < B) System.out.println("<");
+		else System.out.println("==");
+		
+		
+	}
+
+}
